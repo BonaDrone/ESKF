574ddfc7dd7c1f61811eb47d220602c138d178a7
Juan Gallostra <juangallostra@gmail.com>
2019-05-22 17:46:52 +0200

    Implement acelerometer measurement model and its jacobian

--- src/functions/core/accelCorrect.m
+++ src/functions/core/accelCorrect.m
@@ -1,17 +1,35 @@
-function [x,P] = accelCorrect(inputArg1,inputArg2)
+function [x,P] = accelCorrect(x,inputArg2)
 %ACCELCORRECT Summary of this function goes here
 %   Detailed explanation goes here
-    persistent N;
+
+    persistent N; N = [0.7, 0, 0; 0, 0.7, 0; 0, 0, 0.7];
+    persistent g; g = 9.8665;
     
-    %% h(x) - accel. Jacobian of h(x) w.r.t the quaternion
+    % measurement model
+    h = [ g*(2*x(7)*x(9) - 2*x(8)*x(10));
+         -g*(2*x(7)*x(8) + 2*x(9)*x(10));
+         -g*(x(7)^2 - x(8)^2 - x(9)^2 + x(10)^2)];
     
-    R_t = transpose(fromqtoR(q));
-    R_t_g = -R_t*gv;
+    % Jacobian of measurement model 
+    H = [ 0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0,                                   0,                     0;...
+          0, 0, 0, 0, 0, 0,                                 0, - g*x(7)^2 + g*x(8)^2 + g*x(9)^2 - g*x(10)^2, 2*g*x(7)*x(8) + 2*g*x(9)*x(10);...
+          0, 0, 0, 0, 0, 0, g*x(7)^2 - g*x(8)^2 - g*x(9)^2 + g*x(10)^2,                                   0, 2*g*x(7)*x(9) - 2*g*x(8)*x(10);...
+          0, 0, 0, 0, 0, 0,           - 2*g*x(7)*x(8) - 2*g*x(9)*x(10),               2*g*x(8)*x(10) - 2*g*x(7)*x(9),                     0];
     
-    H_x = blkdiag(zeros(6), jacobian(R_t_g, q));
-    X_dx = blkdiag(eye(6), Qmat(q));
+    % h(x) - accel. Jacobian of h(x) w.r.t the quaternion
     
-    H_dx_a = H_x*X_dx;
+    % R_t = transpose(fromqtoR(q));
+    % R_t_g = -R_t*gv;
+    %     
+    % H_x = blkdiag(zeros(6), jacobian(R_t_g, q));
+    % X_dx = blkdiag(eye(6), Qmat(q));
+    %     
+    % H_dx_a = H_x*X_dx;
 
     x = inputArg1;
     P = inputArg2;
