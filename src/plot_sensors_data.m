16a3a8710c80a3c3f56650415f62a5e22e347a9f
Juan Gallostra <juangallostra@gmail.com>
2019-05-30 20:02:08 +0200

    Remove entries with no data from plots

--- src/plot_sensors_data.m
+++ src/plot_sensors_data.m
@@ -3,52 +3,85 @@ format long;
 %% Load raw data
 % try catch structure for debugging
 try
-   data = csvread("../data/raw_data.csv");
+   data = csvread("../data/raw_data_5.csv");
 catch
    % do nothing, just avoid throwing an error
 end
+
+timestamps = data(:,1);
 
 figure;
 % Plot accels
 subplot(4,1,1);
 hold on;
 grid on;
-plot(data(:,1), data(:,2), 'b');
-plot(data(:,1), data(:,3), 'r');
-plot(data(:,1), data(:,4), 'g');
+
+accelx = data(:,2);
+accely = data(:,3);
+accelz = data(:,4);
+
+tx = timestamps(accelx ~= 1000.0);
+ty = timestamps(accely ~= 1000.0);
+tz = timestamps(accelz ~= 1000.0);
+
+accelx = accelx(accelx ~= 1000.0);
+accely = accely(accely ~= 1000.0);
+accelz = accelz(accelz ~= 1000.0);
+
+plot(tx, accelx, 'b');
+plot(ty, accely, 'r');
+plot(tz, accelz, 'g');
 title('accelerations')  
 ylim([-0.2 1.2])
 
 subplot(4,1,2);
 grid on;
-plot(data(:,1), data(:,2), 'b');
+plot(tx, accelx, 'b');
 title('x-axis acceleration (g)')  
 
 subplot(4,1,3);
 grid on;
-plot(data(:,1), data(:,3), 'r');
+plot(ty, accely, 'r');
 title('y-axis acceleration (g)')
 
 subplot(4,1,4);
 grid on;
-plot(data(:,1), data(:,4), 'g');
+plot(tz, accelz, 'g');
 title('z-axis acceleration (g)')
 
 figure;
 % Plot angular velocities
 hold on;
 grid on;
-plot(data(:,1), data(:,5), 'b');
-plot(data(:,1), data(:,6), 'r');
-plot(data(:,1), data(:,7), 'g');
-title('angular velocities (deg/s)')
+gyrox = data(:,5);
+gyroy = data(:,6);
+gyroz = data(:,7);
+
+tx = timestamps(gyrox ~= 1000.0);
+ty = timestamps(gyroy ~= 1000.0);
+tz = timestamps(gyroz ~= 1000.0);
+
+gyrox = gyrox(gyrox ~= 1000.0) * pi/180;
+gyroy = gyroy(gyroy ~= 1000.0) * pi/180;
+gyroz = gyroz(gyroz ~= 1000.0) * pi/180;
+
+plot(tx, gyrox, 'b');
+plot(ty, gyroy, 'r');
+plot(tz, gyroz, 'g');
+title('angular velocities (rad/s)')
 
 
 figure;
 % Plot range height
 subplot(2,1,1)
 grid on;
-plot(data(:,1), data(:,8), 'b');
+range = data(:,8);
+
+tr = timestamps(range ~= 1000.0);
+
+range = range(range ~= 1000.0);
+
+plot(tr, range, 'b');
 title('height (m)')
 
 
@@ -56,6 +89,15 @@ title('height (m)')
 subplot(2,1,2)
 hold on;
 grid on;
-plot(data(:,1), data(:,9), 'b');
-plot(data(:,1), data(:,10), 'r');
+flowx = data(:,9);
+flowy = data(:,10);
+
+tx = timestamps(flowx ~= 1000.0);
+ty = timestamps(flowy ~= 1000.0);
+
+flowx = flowx(flowx ~= 1000.0);
+flowy = flowy(flowy ~= 1000.0);
+
+plot(tx, flowx, 'b');
+plot(ty, flowy, 'r');
 title('flows (accumulated pixels)')
