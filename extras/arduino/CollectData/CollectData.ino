b32c61729ada1b24f0199b3398381245b3a24c2c
Juan Gallostra <juangallostra@gmail.com>
2019-06-05 09:40:11 +0200

    Improve data collection script

--- extras/arduino/CollectData/CollectData.ino
+++ extras/arduino/CollectData/CollectData.ino
@@ -18,58 +18,39 @@
    Copyright (c) 2019 Juan Gallostra
  */
 
-#include <Arduino.h>
+#include "PMW3901.h"
+#include <LSM6DSM.h>
+#include <VL53L1X.h>
+
 #include <Wire.h>
 #include <FS.h>
+#include <Arduino.h>
 
-// Drivers for the sensors
-#include <VL53L1X.h>
-#include <LSM6DSM.h>
-#include <PMW3901.h>
 
-uint32_t LOG_TIME = 35 * 1000000;
-
-// data file
 File datalog;
-uint32_t startTime;
-bool shouldLog = true;
-
-// Data collection frequencies in Hz
-//float IMU_FREQ = 500.0;
-//float ACCEL_FREQ = 100.0;
-//float RANGE_FREQ = 75.0;
-//float FLOW_FREQ = 100.0;
-
-// Update periods
-//float imuUpdatePeriod = 1/IMU_FREQ;
-//float accelUpdatePeriod = 1/ACCEL_FREQ;
-//float rangeUpdatePeriod = 1/RANGE_FREQ;
-//float flowUpdatePeriod = 1/FLOW_FREQ;
-
-uint8_t flowCounter;
-uint8_t FLOW_ITERS = 3; // Read flow each 4 main loop iterations
+// Using digital pin A4 for chip select
+PMW3901 flow(12, &SPI1);
 
 // IMU setup
 // LSM6DSM data-ready interrupt pin
 const uint8_t LSM6DSM_INTERRUPT_PIN = 2;
+// LSM6DSM settings. Options are:
 /*
-    LSM6DSM settings. Options are:
-        - Ascale_t: AFS_2G, AFS_16G, AFS_4G, AFS_8G
-        - Gscale_t: GFS_245DPS, GFS_500DPS, GFS_1000DPS, GFS_2000DPS
-        - Rate_t: ODR_12_5Hz, ODR_26Hz, ODR_52Hz, ODR_104Hz, ODR_208Hz, ODR_416Hz, ODR_833Hz, ODR_1660Hz, ODR_3330Hz, ODR_6660Hz
+    Ascale_t: AFS_2G, AFS_16G, AFS_4G, AFS_8G
+    Gscale_t: GFS_245DPS, GFS_500DPS, GFS_1000DPS, GFS_2000DPS
+    Rate_t: ODR_12_5Hz, ODR_26Hz, ODR_52Hz, ODR_104Hz, ODR_208Hz, ODR_416Hz, ODR_833Hz, ODR_1660Hz, ODR_3330Hz, ODR_6660Hz
 */
 static const LSM6DSM::Ascale_t Ascale = LSM6DSM::AFS_4G;
 static const LSM6DSM::Gscale_t Gscale = LSM6DSM::GFS_2000DPS;
 static const LSM6DSM::Rate_t   AODR   = LSM6DSM::ODR_416Hz;
 static const LSM6DSM::Rate_t   GODR   = LSM6DSM::ODR_416Hz;
-
+// Gyro bias will be estimated by the ESKF filter
 float ACCEL_BIAS[3] = {0.0,0.0,0.0};
 float GYRO_BIAS[3]  = {0.0,0.0,0.0};
-
 // IMU instance
 LSM6DSM _lsm6dsm = LSM6DSM(Ascale, Gscale, AODR, GODR, ACCEL_BIAS, GYRO_BIAS);
 
-bool imuRead(float & _ax,float & _ay,float & _az,float & _gx,float & _gy,float & _gz)
+bool imuRead(float & _ax, float & _ay, float & _az, float & _gx, float & _gy, float & _gz)
 {
     if (_lsm6dsm.checkNewData()) 
     {
@@ -78,6 +59,16 @@ bool imuRead(float & _ax,float & _ay,float & _az,float & _gx,float & _gy,float &
     } 
     return false;
 }
+
+void i2cerror(const char * devicename)
+{
+    while (true) 
+    {
+        Serial.print("Unable to start: ");
+        Serial.println(devicename);
+    }
+}
+
 
 // Rangefinder setup
 VL53L1X _distanceSensor;
@@ -92,32 +83,22 @@ bool distanceAvailable(float & distance)
     return false;
 }
 
-// Optical flow setup
-// Use digital pin 12 for chip select and SPI1 port for comms
-PMW3901 _flowSensor = PMW3901(12, &SPI1);
+const uint8_t blue = 38;
+const uint8_t red = 25;
 
-bool flowAvailable(float & deltaX, float & deltaY)
-{
-    int16_t _deltaX=0, _deltaY=0;
-    _flowSensor.readMotionCount(&_deltaX, &_deltaY);
+uint32_t startTime;
+uint32_t lastFlowTime;
+
+//uint16_t FLOW_OUTLIER = 30;
+
+
+void setup() {
+  
+    pinMode(blue, OUTPUT);
+    digitalWrite(blue, 1);
+    pinMode(red, OUTPUT);
+    digitalWrite(red, 1);
     
-    deltaX = (float)_deltaX;
-    deltaY = (float)_deltaY;
-    return true; 
-}
-
-void i2cerror(const char * devicename)
-{
-    while (true) 
-    {
-        Serial.print("Unable to start: ");
-        Serial.println(devicename);
-    }
-}
-
-void setup(void)
-{
-    // begin the serial port
     Serial.begin(115200);
 
     // IMU setup
@@ -141,97 +122,95 @@ void setup(void)
       case LSM6DSM::ERROR_SELFTEST:
         //i2cerror("failed self-test");
         break;
-
       case LSM6DSM::ERROR_NONE:
         break;
     }
+    
     delay(100);
     // Clear the interrupt
     _lsm6dsm.clearInterrupt();
     _lsm6dsm.calibrate(GYRO_BIAS, ACCEL_BIAS, 127);
 
+
+    if (!flow.begin()) {
+      while(1) { Serial.println("Initialization of the flow sensor failed"); }
+    }
+
     // Start the VL53L1X
     if (!_distanceSensor.begin()) 
     {
-       while (true) Serial.println("Range unavailable");
+      while (true) Serial.println("Range unavailable");
     }
 
-    // Start the PMW3901
-    if (!_flowSensor.begin()) {
-       while (true) Serial.println("Flow unavailable");
-    }
-
-    flowCounter = 0;
-
-    // open file to log data
     DOSFS.begin();
     datalog = DOSFS.open("datalog.csv", "w");
 
+    delay(3000);
+
     startTime = micros();
-
-    pinMode(25, OUTPUT);
-    pinMode(26, OUTPUT);
-
-    digitalWrite(26, LOW);
+    lastFlowTime = micros();
 
 }
 
-void loop(void)
-{
-    // Collect data
-    // Variables to hold IMU data
-    float _ax = 1000.0; // 1000 is an unlikely value to be read from any of the sensors, so it is used to indicate no data
-    float _ay = 1000.0;
-    float _az = 1000.0;
-    float _gx = 1000.0;
-    float _gy = 1000.0;
-    float _gz = 1000.0;
-    // Range data
-    float _d = 1000.0;
-    // Flow data
-    float _fx = 1000.0;
-    float _fy = 1000.0;
+void loop() {
 
-    uint32_t currentTime = micros();
-
+  uint32_t currentTime = micros();
+  
+  if(currentTime - startTime < 180 * 1000000)
+  {
+    
+    digitalWrite(blue, 0);
+    
     // Read sensor data
+    // IMU
+    float _ax, _ay, _az, _gx, _gy, _gz;
+    _ax = 1000.0; // 1000 is an unlikely value to be read from any of the sensors, so it is used to indicate no data
+    _ay = 1000.0;
+    _az = 1000.0;
+    _gx = 1000.0;
+    _gy = 1000.0;
+    _gz = 1000.0;
+
     bool imuData = imuRead(_ax, _ay, _az, _gx, _gy, _gz);
+
+    // Range
+    float _d;
+    _d = 1000.0;
+
     bool rangeData = distanceAvailable(_d);
 
-    flowAvailable(_fx, _fy);
-
-    // Collect all the possible data
-    if (datalog && shouldLog)
+    // Optical Flow
+    int16_t deltaX=1000, deltaY=1000;
+    if (currentTime - lastFlowTime > 200000) // 5Hz
     {
-        digitalWrite(25, HIGH);
-        datalog.print((float)currentTime / 1000000.0, 8);
-        datalog.print(",");
-        datalog.print(_ax, 8);
-        datalog.print(",");
-        datalog.print(_ay, 8);
-        datalog.print(",");
-        datalog.print(_az, 8);
-        datalog.print(",");
-        datalog.print(_gx, 8);
-        datalog.print(",");
-        datalog.print(_gy, 8);
-        datalog.print(",");
-        datalog.print(_gz, 8);
-        datalog.print(",");
-        datalog.print(_d, 8);
-        datalog.print(",");
-        datalog.print(_fx, 8);
-        datalog.print(",");
-        datalog.println(_fy, 8);
-
-        if (currentTime - startTime > LOG_TIME) shouldLog = false;
-
+      flow.readMotionCount(&deltaX, &deltaY);
+      lastFlowTime = currentTime;
     }
-    else
-    {
-        digitalWrite(26, HIGH);
-        digitalWrite(25, LOW);
-        if (datalog) datalog.close();
-    }
+    
+    datalog.print((float)currentTime / 1000000.0, 8);
+    datalog.print(",");
+    datalog.print(_ax, 8);
+    datalog.print(",");
+    datalog.print(_ay, 8);
+    datalog.print(",");
+    datalog.print(_az, 8);
+    datalog.print(",");
+    datalog.print(_gx, 8);
+    datalog.print(",");
+    datalog.print(_gy, 8);
+    datalog.print(",");
+    datalog.print(_gz, 8);
+    datalog.print(",");
+    datalog.print(_d, 8);
+    datalog.print(",");
+    datalog.print(float(deltaX), 8);
+    datalog.print(",");
+    datalog.println(float(deltaY), 8);
 
+  } else {
+
+    digitalWrite(blue, 1);
+    digitalWrite(red, 0);
+    datalog.close();
+  }
 }
